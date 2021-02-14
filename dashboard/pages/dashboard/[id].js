cba7657fd6412da6eacdc31de58446805dfd22b0
Ankit Hans <ankithans1947@gmail.com>
2021-02-14 11:38:18 +0530

    feat(dashboard): improve UI

--- dashboard/pages/dashboard/[id].js
+++ dashboard/pages/dashboard/[id].js
@@ -1,5 +1,21 @@
 import React from "react";
+import SideBar from "../../components/dashboard/sidebar";
 
 export default function note() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <div className="flex flex-row">
+        <SideBar />
+        <div className="p-10">
+          <h1 className="pb-5 font-semibold text-2xl">
+            Class Conversation Recording
+          </h1>
+          <textarea
+            id="meetingText"
+            placeholder="(Meeting text will appear here)"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
 }
--- dashboard/pages/dashboard/home.js
+++ dashboard/pages/dashboard/home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import NavBar from "../../components/dashboard/navbar";
 import HomeBody from "../../components/dashboard/homeBody";
 import SideBar from "../../components/dashboard/sidebar";
 
